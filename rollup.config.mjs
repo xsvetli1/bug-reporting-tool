98e05c506cd594e425f35bb10fb87dda788f5d29
Oliver Svetlík <xsvetli1@fi.muni.cz>
2023-01-20 23:36:56 +0100

    Applying Prettier rules on config files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,39 +1,39 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import json from '@rollup/plugin-json';
 
 export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: "dist/cjs/index.js",
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: "dist/esm/index.js",
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
-      json(),
-    ],
-  },
-  {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/],
-  },
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'dist/cjs/index.js',
+                format: 'cjs',
+                sourcemap: true
+            },
+            {
+                file: 'dist/esm/index.js',
+                format: 'esm',
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfig: './tsconfig.json' }),
+            postcss(),
+            json()
+        ]
+    },
+    {
+        input: 'dist/esm/types/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+        plugins: [dts()],
+        external: [/\.css$/]
+    }
 ];
