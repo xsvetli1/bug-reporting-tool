a9696f236a8c8f68369f86eb3943208cae27f159
Oliver Svetlík <xsvetli1@fi.muni.cz>
2023-01-21 17:32:26 +0100

    Refactoring unneceserry JSX and only returning arrow functions in AnnotationArea

--- src/components/annotations/AnnotationArea/AnnotationArea.tsx
+++ src/components/annotations/AnnotationArea/AnnotationArea.tsx
@@ -17,35 +17,28 @@ export interface AnnotationAreaProps {
 }
 
 const AnnotationArea = ({ selectedAreas, mouseEvents, children }: AnnotationAreaProps) => {
-    const rectToPathData = (selectAreaProps: SelectAreaProps) => {
-        const { x, y, width, height } = selectAreaProps;
-        return `M ${x} ${y} h ${width} v ${height} h ${-width} Z`;
-    };
+    const rectToPathData = ({ x, y, width, height }: SelectAreaProps) =>
+        `M ${x} ${y} h ${width} v ${height} h ${-width} Z`;
 
-    const pathDataFromSelectedAreas = () => {
-        return Object.keys(selectedAreas).map((key: string) => {
+    const pathDataFromSelectedAreas = () =>
+        Object.keys(selectedAreas).map((key: string) => {
             return rectToPathData(selectedAreas[key]);
         });
-    };
 
-    const areaPath = () => {
-        const width = window.innerWidth;
-        const height = window.innerHeight;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-        const background = rectToPathData({
-            x: 0,
-            y: 0,
-            width: width,
-            height: height
-        });
-        const d = [background, ...pathDataFromSelectedAreas()].join('\n');
-
-        return <path fill="#ffffff" fillOpacity="0.3" fillRule="evenodd" d={d}></path>;
-    };
+    const background = rectToPathData({
+        x: 0,
+        y: 0,
+        width: width,
+        height: height
+    });
+    const d = [background, ...pathDataFromSelectedAreas()].join('\n');
 
     return (
         <svg className="annotation-area" {...mouseEvents}>
-            {areaPath()}
+            <path fill="#ffffff" fillOpacity="0.3" fillRule="evenodd" d={d}></path>
             {children}
         </svg>
     );
