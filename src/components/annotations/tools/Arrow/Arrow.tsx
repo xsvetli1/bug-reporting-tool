29a5f7d80fb39c0f6a101fe2645bc352b1f4b21f
Oliver Svetlík <xsvetli1@fi.muni.cz>
2023-02-01 18:37:44 +0100

    Arrow annotation refactoring

--- src/components/annotations/tools/Arrow/Arrow.tsx
+++ src/components/annotations/tools/Arrow/Arrow.tsx
@@ -9,20 +9,17 @@ export interface ArrowProps {
 }
 
 const Arrow = ({ x1, y1, x2, y2 }: ArrowProps) => {
-    const arrowHead = 7 * 4;
+    // TODO: Fix short arrow aiming opposite direction
+    const strokeWidth = 4;
+    const arrowHead = 7 * strokeWidth;
     const vectorX = x2 - x1;
     const vectorY = y2 - y1;
-    console.log('vectorX', vectorX);
-    console.log('vectorY', vectorY);
 
     const signX = vectorX ? vectorX / Math.abs(vectorX) : 1;
     const signY = vectorY ? vectorY / Math.abs(vectorY) : 1;
-    console.log('signX', signX);
-    console.log('signY', signY);
+
     const diffY = vectorY ? Math.sqrt(arrowHead ** 2 / (vectorX ** 2 / vectorY ** 2 + 1)) : 0;
     const diffX = vectorY ? (diffY * Math.abs(vectorX)) / Math.abs(vectorY) : arrowHead;
-    console.log('diffX', diffX);
-    console.log('diffY', diffY);
 
     x2 = x1 + vectorX - signX * diffX;
     y2 = y1 + vectorY - signY * diffY;
@@ -33,7 +30,7 @@ const Arrow = ({ x1, y1, x2, y2 }: ArrowProps) => {
             x2={x2}
             y2={y2}
             stroke="#000"
-            strokeWidth={4}
+            strokeWidth={strokeWidth}
             markerEnd="url(#arrowhead)"
         />
     );
